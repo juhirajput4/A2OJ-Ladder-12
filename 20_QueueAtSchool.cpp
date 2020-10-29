d43550bd36e7811302ff13c261ac3ed3983e95fa
juhirajput4 <70390528+juhirajput4@users.noreply.github.com>
2020-10-29 12:29:14 +0530

    Update 20_QueueAtSchool.cpp

--- 20_QueueAtSchool.cpp
+++ 20_QueueAtSchool.cpp
@@ -1,24 +1,44 @@
-#include<bits\stdc++.h>
-
+#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-    int n,t;
-    string s;
-    cin>>n>>t;
-    cin>>s;
-    while(t--){
-        int i=0;
-        while(i<s.size()){
-            if (s[i]=='B' && s[i+1]=='G'){
-                s[i] = 'G';
-                s[i+1] = 'B';
-                i=i+2;
-                continue;
-            }
-            i++;
+   int ty;
+   cin>>ty;
+   while(ty--)
+   {
+       long long n,h,naman,klpd,wepm=0,f=0;
+       cin>>n>>h>>naman>>klpd;
+       long long upes[n];
+       for(long long i=0;i<n;i++)
+        cin>>upes[i];
+        sort(upes,upes+n);
+        if(upes[naman-1]==h)
+        {
+            cout<<"0"<<endl;
         }
-    }
-    cout<<s<<endl;
+        else{
+            if(upes[naman-1]<h&&naman<klpd)
+                cout<<"-1"<<endl;
+            else if(upes[naman-1]>h&&naman>klpd)
+                cout<<"-1"<<endl;
+            else
+            {
+                 if(upes[naman-1]>h&&naman<=klpd){
+                    int aaram=naman-1;
+                    while(upes[aaram]>h&&aaram>=0){
+                        aaram--;wepm++;}
+                        cout<<wepm<<endl;
+                }
+                else if(upes[naman-1]<h&&naman>=klpd)
+                {
+                    int aaram=naman-1;
+                    while(upes[aaram]<h&&aaram<n){
+                        aaram++;wepm++;}
+                        cout<<wepm<<endl;
+                }
+            }
+        }
+   }
+   return 0;
 }
